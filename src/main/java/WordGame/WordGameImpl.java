dba5107b200d393f5ff4bbc82e34970ef403a98a
Krishna Thapa <krishna.nature91@gmail.com>
2019-02-09 13:00:02 +0000

    Fixed error as per guide lines from feedback

--- src/main/java/WordGame/WordGameImpl.java
+++ src/main/java/WordGame/WordGameImpl.java
@@ -9,29 +9,39 @@ import java.util.*;
  */
 public class WordGameImpl implements WordGame {
 
-    String inputWord;
-    ValidWords validWords = new ValidWordsImpl();
-    public static List<LeaderBoard> leaderList = new ArrayList<>();
+    String inputWords;
+    ValidWords validWords;
+    /*
+    ValidWords validWords = new ValidWordsImpl(); //wrong way
+    I guess assigning the concrete implementation of ValidWords is undesired,
+    we should inject it through the constructor.
+     */
+
+    final List<LeaderBoard> leaderList = new ArrayList<>();
+
+    public List<LeaderBoard> getLeaderList() {
+        return leaderList;
+    }
 
     public WordGameImpl(String inputWords, ValidWords validWords) {
-        this.inputWord = inputWords;
+        this.inputWords = inputWords; //right way inject it through the constructor
+        this.validWords = validWords;
     }
 
     @Override
     public int submitWord(String playerName, String word) {
-        int count = 0;
+        int score = word.length();
 
         //check whether the word is in starting word or not
         Map<Character, Integer> map = new HashMap<>();
-        for(int i =0; i < inputWord.length(); i++){
-            char c = inputWord.charAt(i);
+        for(int i =0; i < inputWords.length(); i++){
+            char c = inputWords.charAt(i);
             map.put(c,map.getOrDefault(c,0)+1);
         }
 
         for(int i = 0; i < word.length(); i++){
             char c = word.charAt(i);
             if(map.containsKey(c) && map.get(c) != 0){
-                count++;
                 map.put(c,map.get(c)-1);
             }else{
                 System.out.println("Error: Number of letters doesn't match to starting word: " + word);
@@ -39,28 +49,22 @@ public class WordGameImpl implements WordGame {
             }
         }
 
-        if(count != word.length()){
-            System.out.println("Error: Number of letters doesn't match to starting word: " + word);
-            return 0;
-        }
-        //System.out.println("count " + count);
-
         //Check whether the word is in text file or not
         if(!validWords.contains(word)){
             System.out.println("Error: Word is not in ValidWords collection text file: " + word);
             return 0;
         }
 
-        LeaderBoard eachScore = new LeaderBoard(playerName,word,count);
-        leaderBoardValidation(eachScore);
+        LeaderBoard submission = new LeaderBoard(playerName,word,score);
+        leaderBoardValidation(submission);
 
-        Collections.sort(leaderList);  //use of Comparable interface to order the object in desc order
+        //Collections.sort(leaderList);  //use of Comparable interface to order the object in desc order
         for(LeaderBoard row : leaderList){
             System.out.println(row.playerName + " " + row.getPlayerWord() + " " + row.getPlayerScore());
         }
         System.out.println();
 
-        return count;
+        return score;
     }
 
     /**
@@ -78,7 +82,15 @@ public class WordGameImpl implements WordGame {
 
         //to check the size of the leader board is always less than 10
         //check if the word has more score than that is listed in leader board
-        if(leaderList.size() < 10){
+        final int maxLeaderBoardLength = 10;
+        leaderList.add(eachScore);
+        Collections.sort(leaderList);
+        System.out.println("Size of the list: "+leaderList.size());
+        if(leaderList.size() > maxLeaderBoardLength){
+            leaderList.remove(maxLeaderBoardLength);
+            return true;
+        }
+        /*if(leaderList.size() < 10){
             leaderList.add(eachScore);
             return true;
         }else{
@@ -94,9 +106,9 @@ public class WordGameImpl implements WordGame {
 
             }
 
-        }
+        }*/
 
-        return false;
+        return true;
     }
     @Override
     public String getPlayerNameAtPosition(int position) {
@@ -117,9 +129,13 @@ public class WordGameImpl implements WordGame {
      * class for the leader board
      */
     public class LeaderBoard implements Comparable<LeaderBoard>{
-        private String playerName;
-        private String playerWord;
-        private int playerScore;
+
+        //It is much safer to make the objects of this class immutable,
+        // all members could be final, no setters are needed.
+
+        private final String playerName;
+        private final String playerWord;
+        private final int playerScore;
 
         public LeaderBoard(String playerName, String playerWord, int playerScore) {
             this.playerName = playerName;
@@ -131,27 +147,16 @@ public class WordGameImpl implements WordGame {
             return playerName;
         }
 
-        public void setPlayerName(String playerName) {
-            this.playerName = playerName;
-        }
-
         public String getPlayerWord() {
             return playerWord;
-        }
-
-        public void setPlayerWord(String playerWord) {
-            this.playerWord = playerWord;
         }
 
         public int getPlayerScore() {
             return playerScore;
         }
 
-        public void setPlayerScore(int playerScore) {
-            this.playerScore = playerScore;
-        }
-
         //use of Comparable interface and override the compareTo() method to sort the object by score in des order
+        @Override
         public int compareTo(LeaderBoard compareScore){
             int compareScoreResult = (compareScore).getPlayerScore();
             return compareScoreResult - this.getPlayerScore();
--- src/test/java/WordGameTest/SubmissionTest.java
+++ src/test/java/WordGameTest/SubmissionTest.java
@@ -66,35 +66,37 @@ public class SubmissionTest {
 
 	@Test
 	public void testSameExactWordReturnFalse() throws Exception {
-		service.submitWord("player2", "word");
-		service.submitWord("player3", "all");
-		service.submitWord("player1", "woolly");
-		service.submitWord("player4", "woolly");
+		WordGameImpl service1 = new WordGameImpl("areallylongword", validWords);
+		service1.submitWord("player2", "word");
+		service1.submitWord("player3", "all");
+		service1.submitWord("player1", "woolly");
+		service1.submitWord("player4", "woolly");
 		//System.out.println(WordGameImpl.leaderList.size());
-		assertEquals(3, WordGameImpl.leaderList.size());
+		assertEquals(3, service1.getLeaderList().size());
 	}
 
 	@Test
 	public void testIfAddedMoreWordsWhenBoardIsFull() throws Exception {
-		service.submitWord("player1", "word");
-		service.submitWord("player2", "all");
-		service.submitWord("player3", "woolly");
-		service.submitWord("player4", "lower");
-		service.submitWord("player5", "loyal");
-		service.submitWord("player6", "orange");
-		service.submitWord("player7", "lawn");
-		service.submitWord("player8", "woolly");
-		service.submitWord("player9", "grey");
-		service.submitWord("player10", "raw");
-		service.submitWord("player11", "laryngeal");
-		service.submitWord("player12", "legally");
+		WordGameImpl service1 = new WordGameImpl("areallylongword", validWords);
+		service1.submitWord("player1", "word");
+		service1.submitWord("player2", "all");
+		service1.submitWord("player3", "woolly");
+		service1.submitWord("player4", "lower");
+		service1.submitWord("player5", "loyal");
+		service1.submitWord("player6", "orange");
+		service1.submitWord("player7", "lawn");
+		service1.submitWord("player8", "woolly");
+		service1.submitWord("player9", "grey");
+		service1.submitWord("player10", "raw");
+		service1.submitWord("player11", "laryngeal");
+		service1.submitWord("player12", "legally");
 
 
 		//System.out.println(WordGameImpl.leaderList.size());
-		assertEquals(10, WordGameImpl.leaderList.size());
-		assertEquals("player11",service.getPlayerNameAtPosition(0));
-		assertEquals("laryngeal",service.getWordEntryAtPosition(0));
-		assertEquals(java.util.Optional.ofNullable(9),java.util.Optional.ofNullable(service.getScoreAtPosition(0)));
+		assertEquals(10, service1.getLeaderList().size());
+		assertEquals("player11",service1.getPlayerNameAtPosition(0));
+		assertEquals("laryngeal",service1.getWordEntryAtPosition(0));
+		assertEquals(java.util.Optional.ofNullable(9),java.util.Optional.ofNullable(service1.getScoreAtPosition(0)));
 
 	}
 }
